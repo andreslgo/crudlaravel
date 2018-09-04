958fbd8efb710bfb780d30d04a1cc8573dcc803e
Andres Luque <andresl@biccofarms.com>
2018-09-04 09:28:29 -0500

    Se crean relaciones en los modelos

--- app/Cities.php
+++ app/Cities.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Cities extends Model
 {
     protected $table = 'cities';
+
+    public function country(){
+    	return $this->belongsTo('App\Countries', 'id_country', 'id');
+    }
 }
--- app/Customers.php
+++ app/Customers.php
@@ -7,5 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Customers extends Model
 {
     protected $table = 'customers';
+
+    public function country(){
+    	return $this->belongsTo('App\Countries', 'id_country', 'id');
+    }
+
+    public function cities(){
+    	return $this->belongsTo('App\Cities', 'id_city', 'id');
+    }
+
+    public function customertypes(){
+    	return $this->belongsTo('App\CustomerTypes', 'id_customer_type', 'id');
+    }
     
 }
